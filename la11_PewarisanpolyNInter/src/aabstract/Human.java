4d153e5823bb301262e3446e44c557aefb1efddb
HERI FIRMANSAH <55747177+Arcviell@users.noreply.github.com>
2019-12-07 00:19:22 +0700

    Add files via upload

---
+++ la11_PewarisanpolyNInter/src/aabstract/Human.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aabstract;
+
+/**
+ *
+ * @author  Heri Firmansah
+ */
+public class Human {
+public static void main(String[] args) {
+}
+   public void walk(){
+ System.out.println("Human walks...");
+ }
+ } 
